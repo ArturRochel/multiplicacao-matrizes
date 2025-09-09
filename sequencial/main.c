3b43c49f1ee14014e330356126caf55a4bd5ee20
arturrochel <arturrochel@gmail.com>
2025-09-09 11:25:32 -0300

    MatrizResultado
    Continuei o desenvolvimento do programa sequencial, desenvolvendo pontos chaves do código. Agora o programa copia as matrizes que estão dentro dos arquivos, confere se é possível realizar o produto entre elas e inicia o processo de contagem do tempo e realização das operações. Os resultados já são adicionados dinamicamente à uma matrizResultado.

--- sequencial/main.c
+++ sequencial/main.c
@@ -17,6 +17,11 @@ int main(int argc, char *argv[]) {
     FILE *arquivoA = fopen(caminhoA, "r");
     FILE *arquivoB = fopen(caminhoB, "r");
 
+    if(arquivoA == NULL || arquivoB == NULL) {
+        printf("Arquivos não foram abertos.");
+        return 1;
+    }
+
     int linhaA = 0;
     int colunaA = 0;
     int linhaB = 0;
@@ -24,8 +29,41 @@ int main(int argc, char *argv[]) {
 
     int itensLidos = 0;
 
-    fscanf(arquivoA, "%d %d", linhaA, colunaA);
-    fscanf(arquivoB, "%d %d", linhaB, colunaB);
+    fscanf(arquivoA, "%d %d", &linhaA, &colunaA);
+    fscanf(arquivoB, "%d %d", &linhaB, &colunaB);
+
+    if(colunaA != linhaB) {
+        printf("As matrizes não podem ser multiplicadas");
+        return 1;
+    }
+
+    int matrizA[linhaA][colunaA];
+    int matrizB[linhaB][colunaB];
+    int matrizResultado[linhaA][colunaB];
+
+    for(int i = 0; i<linhaA; i++) {
+        for(int j = 0; j<colunaA; j++) {
+            fscanf(arquivoA, "%d", &matrizA[i][j]);
+        }
+    }
+
+    for(int i = 0; i<linhaB; i++) {
+        for(int j = 0; j<colunaB; j++) {
+            fscanf(arquivoB, "%d", &matrizB[i][j]);
+        }
+    }
+
+    clock_t inicio;
+    inicio = clock();
+
+    for(int i = 0; i<linhaA; i++) {
+        for(int j = 0; j<colunaB; j++) {
+            matrizResultado[i][j] = 0;
+            for(int k = 0; k<colunaA; k++) {
+                matrizResultado[i][j] = matrizResultado[i][j] + matrizA[i][k] * matrizB[k][j];
+            }
+        }
+    }
 
     return 0;
 }
